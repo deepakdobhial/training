8135000cfaf9cb049697531ccbdd14c157b2142c
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-01-28 18:45:14 +0530

    Add Association: Polymorphic between comments and employees, suppliers, customers tables

--- ruby on rails/projects/inventory_management_system/app/models/customer.rb
+++ ruby on rails/projects/inventory_management_system/app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :comments, as: :commentable
   validates :email, confirmation: true, absence: true, uniqueness: true
   validates :gender, length: { is: 1}
   validates :mobile_no, presence: true, uniqueness: true
