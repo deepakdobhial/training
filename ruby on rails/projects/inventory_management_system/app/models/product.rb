6a1fb8d88dc334856d83eb8337a8bd81e4f580e1
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-01-28 15:55:48 +0530

    Add Association: many-to-many between  'products' and 'brands' tables

--- ruby on rails/projects/inventory_management_system/app/models/product.rb
+++ ruby on rails/projects/inventory_management_system/app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_and_belongs_to_many :brands
   validates :price, comparison: {greater_than: 0}, numericality: true
   validates :name, :brand_id, :category_id, presence: true
 end
