6ffb41560758d76f5b7bf622b05e26b4aa42158e
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-01-28 16:41:32 +0530

    Add Association: many-to-many between 'categories' and 'brands' through 'products' table

--- ruby on rails/projects/inventory_management_system/app/models/brand.rb
+++ ruby on rails/projects/inventory_management_system/app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ApplicationRecord
   has_and_belongs_to_many :products
+  has_many :categories, through: :products
   validates :name, presence: true, uniqueness: true
 end
--- ruby on rails/projects/inventory_management_system/app/models/category.rb
+++ ruby on rails/projects/inventory_management_system/app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
+  has_many :brands, through: :products
   validates :name, presence: true, uniqueness: true
 end
