26eec7c4ad30e344759e42dfe488e8d03dde9c8b
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-01-31 17:41:52 +0530

    Add callback: before_validation

--- ruby on rails/projects/inventory_management_system/app/models/product.rb
+++ ruby on rails/projects/inventory_management_system/app/models/product.rb
@@ -2,5 +2,12 @@ class Product < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :brands
   validates :price, comparison: {greater_than: 0}, numericality: true
-  validates :name, :brand_id, :category_id, presence: true
+  validates :name, :brand_id, :category_id, presence: { strict: true }
+  before_validation :capitalize_name
+
+  private
+  def capitalize_name
+    self.name = name.downcase.titleize
+  end
+  
 end
