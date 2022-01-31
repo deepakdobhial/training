742c6392ed424adf07a08921ed00d2d21dbcdb1a
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-01-31 10:43:05 +0530

    Add validations options: allow_nil, allow_blank, message, on

--- ruby on rails/projects/inventory_management_system/app/models/employee.rb
+++ ruby on rails/projects/inventory_management_system/app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ApplicationRecord
   has_many :comments, as: :commentable
+  validates :name, presence: {message: "Please Enter name"}, on: create
 end
--- ruby on rails/projects/inventory_management_system/app/models/supplier.rb
+++ ruby on rails/projects/inventory_management_system/app/models/supplier.rb
@@ -1,3 +1,5 @@
 class Supplier < ApplicationRecord
   has_many :comments, as: :commentable
+  validates :email, allow_nil: true
+  validates :company_name, allow_blank: true
 end
