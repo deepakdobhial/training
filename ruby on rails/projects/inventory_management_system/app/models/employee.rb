c5543eff0e7bce93aa241b85e3b27405950a73a0
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-01 14:17:06 +0530

    Add Custom Validations

--- ruby on rails/projects/inventory_management_system/app/models/employee.rb
+++ ruby on rails/projects/inventory_management_system/app/models/employee.rb
@@ -1,4 +1,14 @@
 class Employee < ApplicationRecord
   has_many :comments, as: :commentable
-  validates :name, presence: {message: "Please Enter name"}, on: create
+  validates :name, presence: {message: "can't be blank"}
+  validate  :dob_validate
+  
+  private
+  def dob_validate
+    age = ((Time.zone.now - self.date_of_birth.to_time) / (1.year.seconds)).to_i
+    if age < 18
+      self.errors.add :date_of_birth, "The age of a employee must be 18 or older"
+    end
+  end
+    
 end
