de72b4f5318cfb8c07f5b94a9ad3d6cc01d6f2d3
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-01 18:33:00 +0530

    Add callback: before_create and after_create

--- ruby on rails/projects/inventory_management_system/app/models/category.rb
+++ ruby on rails/projects/inventory_management_system/app/models/category.rb
@@ -1,5 +1,18 @@
 class Category < ApplicationRecord
   has_many :products
   has_many :brands, through: :products
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { in: 2..30 }
+  before_create :remove_whitespaces
+  after_create :capitalize_name
+
+  private 
+  def remove_whitespaces
+    self.name = name.strip
+  end
+
+  private
+  def capitalize_name
+    self.name = name.downcase.titleize
+  end
+   
 end
