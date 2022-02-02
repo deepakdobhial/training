e337f461ee71196623b678243846d9fc5ae79655
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-02 10:46:46 +0530

    Add callbacks: before_save, around_save, after_save

--- ruby on rails/projects/inventory_management_system/app/models/department.rb
+++ ruby on rails/projects/inventory_management_system/app/models/department.rb
@@ -1,2 +1,20 @@
 class Department < ApplicationRecord
+  validates :name, presence: {message: "can't be blank"}
+  before_save :capitalize_name
+  around_save :remove_whitespaces
+  after_save :upcase_name
+  
+  def capitalize_name
+    self.name = name.titleize
+  end
+
+  def remove_whitespaces
+    self.name.strip!
+    yield
+  end
+
+  def upcase_name
+    self.name.upcase!
+  end
+
 end
