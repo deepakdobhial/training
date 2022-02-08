ed94998bdf3bf15bb37e1aa4ff1fd1dd42f6d4e7
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-08 19:04:32 +0530

    Update routes

--- ruby on rails/projects/inventory_management_system/config/routes.rb
+++ ruby on rails/projects/inventory_management_system/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :orders
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,8 +11,16 @@ Rails.application.routes.draw do
   get "brands/:id/" => "products#brands_wise_products"
   get "products/categories_wise_details/:name" => "products#categories_wise_details"
   get "brands/:id/products/brands_wise_details/:name" => "products#brands_wise_details"
-  get "customers/" => "customers#index"
-  get "customers/:id/" => "customers#show"
-  get "/form/" => "customers#form"
-  post "/customers/create"
+  get "customers" => "customers#index"
+  get "customers/new" => "customers#new" 
+  get "customers/:id" => "customers#show"
+  post "customers/" => "customers#create"
+  get "customers/:id/edit" => "customers#edit"
+  resources :customers
+  # patch "customers/update" => "customers#update"
+  # delete "customers/destroy/:id" => "customers#destroy"
+  
+  
+  
+  
 end
