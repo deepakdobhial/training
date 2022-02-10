4d5fa31800b5037477e72609f2f519be5c096754
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-10 15:38:44 +0530

    Add Validations in User and Product Model

--- ruby on rails/assignment/product_management_system/app/models/product.rb
+++ ruby on rails/assignment/product_management_system/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
 belongs_to :user, foreign_key: true
+validetes :name, presence: true
+validates :price, presence: true, numericality: true
 end
--- ruby on rails/assignment/product_management_system/app/models/user.rb
+++ ruby on rails/assignment/product_management_system/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :products
+  validates :user, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :date_of_birth, presence: true
 end
