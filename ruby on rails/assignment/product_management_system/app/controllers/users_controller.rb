e11e6ca90b063e48e37a8075fba01aa614913fbd
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-11 18:10:29 +0530

    Add user contoller and create actions: signup, signin, logout, create

---
+++ ruby on rails/assignment/product_management_system/app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def signup
+    @user = User.new
+  end
+
+  def signin
+    @user = User.find_by(name: params[:name], password: params[:password])
+    if @user.nil?
+      flash[:message] = "Wrong username or password"
+      redirect_to "/" 
+    else
+      session[:current_user_id] = @user.id
+      redirect_to products_path
+    end
+  end
+
+  def logout
+    session.delete(:current_user_id)
+    redirect_to "/"
+  end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:message] = "Register Successfully"
+      redirect_to "/"
+    else
+      render :signup, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:name, :password, :password_confirmation, :date_of_birth, :address, :terms_of_services)
+    
+  end
+
+end
