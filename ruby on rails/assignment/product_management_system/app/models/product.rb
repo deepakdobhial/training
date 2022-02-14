ee1ab232ef370f4b7366393e2ea7667b76430855
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-14 10:29:59 +0530

    Add callback: after_validation in 'Product' model

--- ruby on rails/assignment/product_management_system/app/models/product.rb
+++ ruby on rails/assignment/product_management_system/app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
-belongs_to :user, foreign_key: true
-validetes :name, presence: true
-validates :price, presence: true, numericality: true
+  belongs_to :user
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
+  after_validation :change_to_downcase
+
+  private
+  def change_to_downcase
+    self.name = self.name.downcase
+  end
+
 end
