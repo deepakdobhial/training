8c27c8f257c1180463f09e01eb43411cc71d5156
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-14 11:56:52 +0530

    Update routes

--- ruby on rails/assignment/product_management_system/config/routes.rb
+++ ruby on rails/assignment/product_management_system/config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get "signup" => "users#signup"
+  post "create" => "users#create"
+  get "signin" => "users#signin"
+  get "users/signout" => "users#signout"
+  get "products/" => "products#index"
+  get "products/new" => "products#new"
+  post "products/create" => "products#create"
+  get "products/edit/:id" => "products#edit"
+  patch "products/update/:id" => "products#update"
+  get "products/:id" => "products#delete"
+  delete "products/:id" => "products#delete"
+    
 end
