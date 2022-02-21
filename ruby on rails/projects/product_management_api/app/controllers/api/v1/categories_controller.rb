2e9ad260133b3531d04a65975b71fe763be8e1b7
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-21 11:55:52 +0530

    Create categories controller with index, create, show, destroy, update actions "

---
+++ ruby on rails/projects/product_management_api/app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::CategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @categories = Category.all
+    render json: {data: @categories}
+  end
+  
+  def create
+    @category = Category.create(category_params)
+    render json: {data: @category}
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    render json: {data: @category}
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    if @category.present?
+      @category.destroy
+      render json: {data: "Deleted"}
+    else
+      render json: {data: "category not found"}
+    end
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.update(category_params)
+    render json: {data: @category}
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
