d171db75de774dbca3f183c625d6707a0999e2ba
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-22 11:11:22 +0530

    Update products controllers actions

--- ruby on rails/projects/product_management_api/app/controllers/api/v1/products_controller.rb
+++ ruby on rails/projects/product_management_api/app/controllers/api/v1/products_controller.rb
@@ -2,40 +2,75 @@ class Api::V1::ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index 
-    @products = Product.all
-    render json: {data: @products}
+    category = Category.find_by(id: params[:category_id])
+    if category.present? 
+      @products = category.products
+    else
+      @products = Product.all
+    end
+
+    render json: {data: ActiveModelSerializers::SerializableResource.new(@products, each_serializer: ProductSerializer)}
   end
 
   def create
-    @product = Product.create(product_params)
-    render json: {data: @product}
-  end
+      category = Category.find_by(id: params[:category_id])
+      if category.present?
+        @product = category.products.create(product_params)
+        render json: {data: @product}
+      else
+        render json: {message: "wrong category id "}
+      end
+    end
 
-  def update 
-    @product = Product.find_by(id: params[:id])
-    @product.update(product_params)
-    render json: {data: @product}
+  def update
+    category = Category.find_by(id: params[:category_id])
+    if category.present?
+      @product = category.products.find_by(id: params[:id])
+    else 
+      @product = Product.find_by(id: params[:id])
+    end
+    
+    if @product.nil?
+      render json: {status: "ERROR", message: "Product not found"} 
+    else
+      @product.update(product_params)
+      render json: {status: status, message: "Product updated", data: @product}
+    end
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
-    render json: {data: @product}
+    if params[:id].present? && params[:category_id].present?
+      @product = Product.find_by(id: params[:id], category_id: params[:category_id])
+    else
+      @product = Product.find_by(id: params[:id])
+    end
+
+    if @product.nil?
+      render json: {status: "ERROR", message: "Product not found"} 
+    else
+      render json: {status: status, message: "OK", data: @product}
+    end
   end
 
   def destroy 
-    @product = Product.find_by(id: params[:id])
+    category = Category.find_by(id: params[:category_id])
+    if category.present?
+      @product = category.products.find_by(id: params[:id])
+    else
+      @product = Product.find_by(id: params[:id])
+    end
+    
     if @product.present?
       @product.destroy
-      render json: {data: "Product Deleted"}
+      render json: {message: "Product Deleted successfully"}
     else 
-      render json: {data: "Product not found"}
+      render json: {message: "Product not found"}
     end
   end
         
   private
   def product_params
-    params.require(:product).permit(:name, :price, :category_id)
+    params.require(:product).permit(:name, :price)
   end
-
 
 end
