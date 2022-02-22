fd9f466e13545f5862329139ab46b73c09f651da
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-22 11:18:17 +0530

    Update routes

--- ruby on rails/projects/product_management_api/config/routes.rb
+++ ruby on rails/projects/product_management_api/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      resources :categories do 
+        resources :products
+      end
       resources :products
-      resources :categories
     end
   end
 
