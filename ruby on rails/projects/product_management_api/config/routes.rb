edb2f91038e73a82a8c3e6cde9fc3cf3c20dbcae
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-23 12:59:30 +0530

    Add versioning through customer header

--- ruby on rails/projects/product_management_api/config/routes.rb
+++ ruby on rails/projects/product_management_api/config/routes.rb
@@ -1,18 +1,16 @@
+require 'api_constraints'
 Rails.application.routes.draw do
   
-  namespace :api do
-    namespace :v1 do
+  namespace :api, default: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :categories do 
         resources :products
       end
       resources :products
     end
-  end
-
-  namespace :api do 
-    namespace :v2 do
-      resources :products
+    scope module: :v2, constraints: ApiConstraints.new(version: 2) do
       resources :brands
+      resources :products
     end
   end
   
