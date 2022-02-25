6cb13577c00dfae5b4ddfb25a54d3e83a12e6b8b
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-25 11:33:21 +0530

    Insert data into products table using seeds

--- ruby on rails/projects/product_ajax/db/seeds.rb
+++ ruby on rails/projects/product_ajax/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+j = 200
+50.times do |i|
+  products = Product.create(name: "Product-#{i}", price: "#{j*i}")
+  j += 10 
+end
+
