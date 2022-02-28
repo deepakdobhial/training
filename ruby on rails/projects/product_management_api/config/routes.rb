c8dec1efd061f1c7e24c5890e18a630569575169
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-28 16:46:27 +0530

    Add apipie routes

--- ruby on rails/projects/product_management_api/config/routes.rb
+++ ruby on rails/projects/product_management_api/config/routes.rb
@@ -1,17 +1,32 @@
 require 'api_constraints'
 Rails.application.routes.draw do
+  apipie
   
-  namespace :api, default: {format: 'json'} do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  # namespace :api, default: {format: 'json'} do
+  #   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  #     resources :categories do 
+  #       resources :products
+  #     end
+  #     resources :products
+  #   end
+  #   scope module: :v2, constraints: ApiConstraints.new(version: 2) do
+  #     resources :brands
+  #     resources :products
+  #   end
+  # end
+  namespace :api do
+    namespace :v1 do 
       resources :categories do 
         resources :products
       end
       resources :products
     end
-    scope module: :v2, constraints: ApiConstraints.new(version: 2) do
+
+    namespace :v2 do 
       resources :brands
       resources :products
     end
+    
   end
   
 end
