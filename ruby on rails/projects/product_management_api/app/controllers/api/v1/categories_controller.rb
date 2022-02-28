bcb635a03bb2c41726b99b5b5ce40ca820a91a22
deepakdobhial <deepak.dobihal@berylsystems.com>
2022-02-28 17:46:32 +0530

    Add apipie param_group, api:[GET,POST,PUT,DELETE]  on categories controller

--- ruby on rails/projects/product_management_api/app/controllers/api/v1/categories_controller.rb
+++ ruby on rails/projects/product_management_api/app/controllers/api/v1/categories_controller.rb
@@ -1,21 +1,37 @@
 class Api::V1::CategoriesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def_param_group :category do
+    param :id, Integer, :desc => "Category id", :required => true
+    param :name, String, :desc => "Category name", :required => true
+  end
+     
+  # api!
+  api :GET, '/v1/categories/', "Show all Categories"
   def index
     @categories = Category.all
     render json: {data: ActiveModelSerializers::SerializableResource.new(@categories, each_serializer: CategorySerializer)}
   end
   
+  # api!
+  api :POST, '/v1/categories/', "Create new a Category"
+  param_group :category  
   def create
     @category = Category.create(category_params)
     render json: {data: @category}
   end
 
+  # api!
+  api :GET, '/v1/categories/:id', "Show a Category"
+  param :id, Integer, :desc => "Category id", :required => true
   def show
     @category = Category.find_by(id: params[:id])
     render json: {data: @category}
   end
 
+  
+  api :DELETE, '/v1/categories/:id', "Delete a Product"
+  param :id, Integer, :desc => "Category id", :required => true
   def destroy
     @category = Category.find_by(id: params[:id])
     if @category.present?
@@ -26,6 +42,9 @@ class Api::V1::CategoriesController < ApplicationController
     end
   end
 
+  
+  api :PUT, '/v1/categories/:id', "Update a Product"
+  param_group :category
   def update
     @category = Category.find_by(id: params[:id])
     @category.update(category_params)
